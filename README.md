76f80fbfe5c3e95926e454bbe1680e180625c804
JamesCurtis <jamescurtis@foxmail.com>
2023-11-22 22:43:41 +0800

    chore: update readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# 2023-ctes.cn-uni
+
+## UI
+
+https://www.figma.com/file/KFJ6plC4l06xDcsPDMPZaP
+
+## 工具
+
+如果使用 webstorm 开发，推荐插件
+- Uniapp tool
--- types/shime-uni.d.ts
+++ types/shime-uni.d.ts
@@ -5,10 +5,4 @@ declare module '@vue/runtime-core' {
   type Hooks = App.AppInstance & Page.PageInstance
 
   interface ComponentCustomOptions extends Hooks {}
-
-  interface GlobalComponents {
-    view: HTMLElement
-    image: HTMLElement
-    text: HTMLElement
-  }
 }
