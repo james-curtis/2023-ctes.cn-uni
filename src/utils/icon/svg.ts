67413cc2712674c00bcd1e323a0acfd567be6957
JamesCurtis <jamescurtis@foxmail.com>
2023-12-07 00:33:23 +0800

    fix: import svg 微调

--- src/utils/icon/svg.ts
+++ src/utils/icon/svg.ts
@@ -1,14 +1,15 @@
 export function useSvgDataUrl(path: string) {
+  const dir = `/src/assets/svg`
   const svgMapper: Record<string, string> = import.meta.glob(
-    '/src/assets/svg/**/*.svg',
+    `/src/assets/svg/**/*.svg`,
     {
       eager: true,
       as: 'raw',
     }
   )
-  if (!svgMapper[`/src/assets/svg/${path}`]) {
+  if (!svgMapper[`${dir}/${path}`]) {
     throw new Error(`svg not found: ${path}`)
   }
-  const content = svgMapper[`/src/assets/svg/${path}`]
+  const content = svgMapper[`${dir}/${path}`]
   return `data:image/svg+xml,${encodeURIComponent(content)}`
 }
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
