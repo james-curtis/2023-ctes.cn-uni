7b7b80a6e46c4342b84c55bd9332bf3f1ef9d22b
JamesCurtis <jamescurtis@foxmail.com>
2023-12-07 21:31:58 +0800

    fix: uCharts

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,42 @@
 <template>
-  <default> <text>asdf</text> </default>
+  <default>
+    <view class="charts-box">
+      <qiun-data-charts type="column" :chart-data="chartData" />
+    </view>
+  </default>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import Default from '@/layout/default.vue'
 
 const title = ref('Hello')
+const chartData = ref()
+
+onMounted(() => {
+  //模拟从服务器获取数据时的延时
+  setTimeout(() => {
+    const res = {
+      categories: ['2016', '2017', '2018', '2019', '2020', '2021'],
+      series: [
+        {
+          name: '目标值',
+          data: [35, 36, 31, 33, 13, 34],
+        },
+        {
+          name: '完成量',
+          data: [18, 27, 21, 24, 6, 28],
+        },
+      ],
+    }
+    chartData.value = JSON.parse(JSON.stringify(res))
+  }, 500)
+})
 </script>
 
-<style></style>
+<style lang="scss">
+.charts-box {
+  width: 100%;
+  height: 300px;
+}
+</style>
