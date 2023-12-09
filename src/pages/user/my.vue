e6d64fe2ea04237b988b83033fb2492aba782774
JamesCurtis <jamescurtis@foxmail.com>
2023-12-09 18:50:06 +0800

    feat: my 页面

--- src/pages/user/my.vue
+++ src/pages/user/my.vue
@@ -1,9 +1,19 @@
 <template>
-  <default>1</default>
+  <default>
+    <view>
+      <tm-image
+        :width="60"
+        :height="60"
+        src="https://picsum.photos/100"
+        unit="px"
+      />
+    </view>
+  </default>
 </template>
 
 <script setup lang="ts">
 import Default from '@/layout/default.vue'
+import TmImage from '@/tmui/components/tm-image/tm-image.vue'
 </script>
 
 <style lang="scss" scoped></style>
