2d184a7ba0e329a3815490560d0d9e314d554d8c
hyh <115272196+coder7986@users.noreply.github.com>
2023-12-12 20:30:40 +0800

    feat: my 页面

--- src/pages/user/my.vue
+++ src/pages/user/my.vue
@@ -2,39 +2,68 @@
   <default>
     <view class="mb-32 mx-32">
       <tm-image
+        :round="25"
         :width="60"
         :height="60"
+        errorLabel='加载错误'
         src="https://picsum.photos/100"
         unit="px"
       />
     </view>
-
-    <view class="mb-32 mx-32 round-3 overflow">
+    <tm-divider :border="10" color="black"></tm-divider>
+    <view class="mb-32 mx-32 round-5 overflow">
       <tm-cell
-        bottom-border
+        showAvatar
+        :avatar="icon.account"
+        color="rgb(245,245,245)"
+        bottomBorder
         :margin="[0, 0]"
-        :title-font-size="30"
+        :titleFontSize="30"
         title="账号"
-      />
+      >
+      </tm-cell>
+      <!-- <tm-divider color="green"></tm-divider> -->
       <tm-cell
-        bottom-border
+        showAvatar
+        :avatar="icon.fav"
+        color="rgb(245,245,245)"
+        bottomBorder
         :margin="[0, 0]"
-        :title-font-size="30"
+        :titleFontSize="30"
         title="收藏"
-      />
+      >
+      </tm-cell>
       <tm-cell
-        bottom-border
+        showAvatar
+        :avatar="icon.contact"
+        color="rgb(245,245,245)"
+        bottomBorder
         :margin="[0, 0]"
-        :title-font-size="30"
+        :titleFontSize="30"
         title="客服"
-      />
-      <tm-cell :margin="[0, 0]" :title-font-size="30" title="设置" />
+      >
+      </tm-cell>
+      <tm-cell
+        showAvatar
+        :avatar="icon.settings"
+        color="rgb(245,245,245)"
+        bottomBorder
+        :margin="[0, 0]"
+        :titleFontSize="30"
+        title="设置"
+      >
+      </tm-cell>
     </view>
   </default>
 </template>
 
 <script setup lang="ts">
 import Default from '@/layout/default.vue'
+import tmDivider from '@/tmui/components/tm-divider/tm-divider.vue'
+import { icons } from '@/config/icons'
+import { reactive } from 'vue'
+
+const icon = reactive(icons)
 </script>
 
 <style lang="scss" scoped></style>
